4da908888d019ab25c1da758bb4cbcf1a2c5ab7c
Ebenezer2077 <tobigcb547@outlook.de>
2023-11-23 16:16:43 +0100

    Login is Done

--- app/src/main/java/com/dk4max/HS_Esslingen/communitygroups/MainActivity.java
+++ app/src/main/java/com/dk4max/HS_Esslingen/communitygroups/MainActivity.java
@@ -1,8 +1,13 @@
 package com.dk4max.HS_Esslingen.communitygroups;
 
+import android.content.Intent;
 import android.os.Bundle;
+
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -16,7 +21,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.dk4max.HS_Esslingen.communitygroups.databinding.ActivityMainBinding;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etPassword;
+    private EditText etUsername;
+    private Button btnLogin;
+
+
+
+
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
@@ -47,6 +64,19 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+
+        etPassword = findViewById(R.id.editTextTextPassword);
+        etUsername = findViewById(R.id.editTextText);
+        btnLogin = findViewById(R.id.button);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getAccessToken();
+            }
+        });
     }
 
     @Override
@@ -61,5 +91,38 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+
+
+    public void getAccessToken() {
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+
+        String password = etPassword.getText().toString();
+        String username = etUsername.getText().toString();
+        String client_secret = "Hier steht das client secret";
+
+        Call<AccessToken> call = service.getAccessToken("AppLogin", "password", client_secret, "openid", username, password);
+
+        call.enqueue(new Callback<AccessToken>() {
+            @Override
+            public void onResponse(Call<AccessToken> call, Response<AccessToken> response) {
+                if(response.isSuccessful()) {
+                    AccessToken accesToken = response.body();
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    MainActivity.this.startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Errortest1243", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AccessToken> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Error45", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 }
